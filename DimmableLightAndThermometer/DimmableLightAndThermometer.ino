8ae112aa0e3c7217a7935a7e57b6f45cb3b0936c
Philippe Weidmann <philweidmann@me.com>
2021-10-09 21:39:24 +0200

    CO2 Sensor | Remove Bluetooth | Formatting | Double

--- DimmableLightAndThermometer/DimmableLightAndThermometer.ino
+++ DimmableLightAndThermometer/DimmableLightAndThermometer.ino
@@ -6,9 +6,6 @@
 #include <ArduinoOTA.h>
 #include <ArduinoJson.h>
 #include <WebSocketsClient.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEBeacon.h>
 
 const char* ssid = "";
 const char* password = "";
@@ -23,17 +20,17 @@ const char * host = "";
 #define PWM_RES 8
 #define PWM_GPIO 4
 
-#define beaconID 306
-#define beaconMinor 2
-#define deviceTypeBeacon "beacon"
-#define SERVICE_UUID "2D7A9F0C-E0E8-4CC9-A71B-A21DB2D034A1"
-
 #define thermometerID 307
 #define deviceTypeThermometer "thermometer"
-const int oneWireBus = 5;     
+const int oneWireBus = 5;
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
+
+#define co2SensorID 503
+#define deviceTypeCO2Sensor "co2sensor"
+
 unsigned long lastUpdate;
+unsigned int flip = 0;
 
 int currentState = 0;
 int percentToVal(int val) {
@@ -57,23 +54,6 @@ String RegisterPacket() {
   return output;
 }
 
-String RegisterPacketBeacon() {
-  const size_t capacity = 2 * JSON_OBJECT_SIZE(3);
-  DynamicJsonDocument doc(capacity);
-
-  doc["type"] = 2;
-
-  JsonObject data = doc.createNestedObject("data");
-  data["deviceId"] = beaconID;
-  data["deviceType"] = deviceTypeBeacon;
-  data["deviceValue"] = beaconMinor;
-  
-
-  String output;
-  serializeJson(doc, output);
-  return output;
-}
-
 String RegisterPacketThermometer() {
   const size_t capacity = 2 * JSON_OBJECT_SIZE(2);
   DynamicJsonDocument doc(capacity);
@@ -83,23 +63,53 @@ String RegisterPacketThermometer() {
   JsonObject data = doc.createNestedObject("data");
   data["deviceId"] = thermometerID;
   data["deviceType"] = deviceTypeThermometer;
-  
+
 
   String output;
   serializeJson(doc, output);
   return output;
 }
 
-String UpdatePacketThermometer(int temperature) {
-  const size_t capacity = 2*JSON_OBJECT_SIZE(2);
-  DynamicJsonDocument doc(capacity);
-  
+String UpdatePacketThermometer(float temperature) {
+  StaticJsonDocument<64> doc;
+
   doc["type"] = 1;
-  
+
   JsonObject data = doc.createNestedObject("data");
   data["deviceId"] = thermometerID;
   data["deviceValue"] = temperature;
-  
+
+  String output;
+  serializeJson(doc, output);
+  return output;
+}
+
+String RegisterPacketCO2Sensor() {
+  const size_t capacity = 2 * JSON_OBJECT_SIZE(2);
+  DynamicJsonDocument doc(capacity);
+
+  doc["type"] = 2;
+
+  JsonObject data = doc.createNestedObject("data");
+  data["deviceId"] = co2SensorID;
+  data["deviceType"] = deviceTypeCO2Sensor;
+
+
+  String output;
+  serializeJson(doc, output);
+  return output;
+}
+
+String UpdatePacketCO2Sensor(int ppm) {
+  const size_t capacity = 2 * JSON_OBJECT_SIZE(2);
+  DynamicJsonDocument doc(capacity);
+
+  doc["type"] = 1;
+
+  JsonObject data = doc.createNestedObject("data");
+  data["deviceId"] = co2SensorID;
+  data["deviceValue"] = ppm;
+
   String output;
   serializeJson(doc, output);
   return output;
@@ -153,10 +163,10 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
       packet = RegisterPacket();
       webSocket.sendTXT(packet);
 
-      packet = RegisterPacketBeacon();
+      packet = RegisterPacketThermometer();
       webSocket.sendTXT(packet);
 
-      packet = RegisterPacketThermometer();
+      packet = RegisterPacketCO2Sensor();
       webSocket.sendTXT(packet);
       break;
     case WStype_TEXT:
@@ -176,40 +186,17 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
   }
 
 }
-void initBluetooth() {
-  BLEDevice::init("");
-  BLEServer *pServer = BLEDevice::createServer();
-  
-  BLEBeacon myBeacon = BLEBeacon();
-  myBeacon.setManufacturerId(0x4C00);
-  myBeacon.setMajor(1);
-  myBeacon.setMinor(beaconMinor);
-  BLEUUID bleUUID = BLEUUID(SERVICE_UUID) ;
-  bleUUID = bleUUID.to128();
-  myBeacon.setProximityUUID(BLEUUID( bleUUID.getNative()->uuid.uuid128, 16, true ));
-  myBeacon.setSignalPower(0xc5);
- 
-  BLEAdvertisementData advertisementData;
-  advertisementData.setFlags(0x1A);
-  advertisementData.setManufacturerData(myBeacon.getData());
- 
-  BLEAdvertising* pAdvertising = pServer->getAdvertising();
-  pAdvertising->setAdvertisementData(advertisementData);
- 
- 
-  pAdvertising->start();
-}
 
 void setup() {
   Serial.begin(115200);
   ledcSetup(PWM_CHANNEL, PWM_FREQ, PWM_RES);
   ledcAttachPin(PWM_GPIO, PWM_CHANNEL);
-  
+
   Serial.println("Booting");
   char hostname[32];
   snprintf(hostname, 32, "ESP32-%d", ID);
   WiFi.mode(WIFI_STA);
-  WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
+  //WiFi.config(INADDR_NONE, INADDR_NONE, INADDR_NONE);
   WiFi.setHostname(hostname);
   WiFi.begin(ssid, password);
   while (WiFi.waitForConnectResult() != WL_CONNECTED) {
@@ -219,30 +206,30 @@ void setup() {
   }
 
   ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
+  .onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
 
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  })
+  .onEnd([]() {
+    Serial.println("\nEnd");
+  })
+  .onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  })
+  .onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
 
   ArduinoOTA.setHostname(hostname);
   ArduinoOTA.begin();
@@ -251,9 +238,9 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  initBluetooth();
   sensors.begin();
-  
+  Serial1.begin(9600, SERIAL_8N1, 25, 26);  // RX, TX
+
   webSocket.begin(host, port, "/", "");
   webSocket.onEvent(webSocketEvent);
   webSocket.setReconnectInterval(5000);
@@ -263,12 +250,58 @@ void loop() {
   ArduinoOTA.handle();
   webSocket.loop();
   ledcWrite(PWM_CHANNEL, currentState);
-  if (lastUpdate + 5 * 1000 < millis()) {
-      lastUpdate = millis();
+  if (lastUpdate + 15 * 1000 < millis()) {
+    lastUpdate = millis();
+    String packet;
+    if (flip == 0) {
+      flip = 1;
       sensors.requestTemperatures();
-      int temperatureC = (int) sensors.getTempCByIndex(0);
-      String packet =  UpdatePacketThermometer(temperatureC);
-      webSocket.sendTXT(packet);
+      float temperatureC = sensors.getTempCByIndex(0);
+      Serial.print("Temperature measured: ");
+      Serial.println(temperatureC);
+      packet = UpdatePacketThermometer(temperatureC);
+    } else {
+      flip = 0;
+      int ppm = getCO2();
+      Serial.print("Co2 measured: ");
+      Serial.println(ppm);
+      packet = UpdatePacketCO2Sensor(ppm);
+    }
+    webSocket.sendTXT(packet);
   }
- 
+
+}
+
+byte CO2req[] = {0xFE, 0X44, 0X00, 0X08, 0X02, 0X9F, 0X25};
+byte CO2out[] = {0, 0, 0, 0, 0, 0, 0};
+
+int getCO2() {
+  while (!Serial1.available())
+  {
+    Serial1.write(CO2req, 7);
+    delay(50);
+  }
+
+  int timeout = 0;
+  while (Serial1.available() < 7 )
+  {
+    timeout++;
+    if (timeout > 10)
+    {
+      while (Serial1.available())
+        Serial1.read();
+      break;
+    }
+    delay(50);
+  }
+
+  for (int i = 0; i < 7; i++)
+  {
+    CO2out[i] = Serial1.read();
+  }
+
+  int high = CO2out[3];
+  int low = CO2out[4];
+  unsigned long val = high * 256 + low;
+  return val * 1; // S8 = 1. K-30 3% = 3, K-33 ICB = 10
 }
